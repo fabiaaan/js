daaa515da26a288302e1588291694474c1ead041
Fabian <fabiaandev@gmail.com>
2021-02-04 02:50:54 -0500

    Add learning (#1)

---
+++ 01-tipos-de-datos/main.js
@@ -0,0 +1,6 @@
+let number = 5
+let word = 'Hello world'
+let response = true
+const PI = 3.14
+
+console.log(PI)
---
+++ 02-tipos-de-operadores/main.js
@@ -0,0 +1,36 @@
+// Mathematical operators
+let a = 5
+let b = 2
+let user = 'Mia'
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+console.log(`Hola ${user}`)
+
+// Increment and decrement operators
+let i = 5
+
+console.log(i++)
+console.log(i)
+
+let d = 8
+
+console.log(d--)
+console.log(d)
+
+// Assignment operators
+let number = 10
+
+number += 3
+console.log(number)
+number -= 3
+console.log(number)
+number *= 3
+console.log(number)
+number /= 3
+console.log(number)
+number %= 3
+console.log(number)
